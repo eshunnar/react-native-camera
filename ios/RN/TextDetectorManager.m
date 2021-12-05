112452791cf5c8f45fd3219dd5e1146fbb761780
Enrique Shunnar <xtealer00@gmail.com>
2021-12-05 14:27:52 -0500

    add missing imports for TextDetectorManager

--- ios/RN/TextDetectorManager.m
+++ ios/RN/TextDetectorManager.m
@@ -1,6 +1,11 @@
 #import "TextDetectorManager.h"
 #if __has_include(<MLKitTextRecognition/MLKitTextRecognition.h>)
 @import MLKitVision;
+//add all imports
+@import MLKitTextRecognition;
+@import MLKitTextRecognitionCommon;
+@import MLCompute;
+@import MLKitVision;
 
 @interface TextDetectorManager ()
 @property(nonatomic, strong) MLKTextRecognizer *textRecognizer;
